5b13beaa2a04b2d08943c23fda3a5379b8eec6c4
GoranRadmanovic <goranradmanovic@gmail.com>
2021-05-05 11:43:39 +0200

    Small fix

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,7 +7,6 @@
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
             <div class="inline-block align-bottom bg-white rounded-md text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full lg:w-1/2">
-            
                 <div class="p-3 text-right">
                     <button type="button" @click="closeModal" class="mt-3 w-full inline-flex justify-center border-0 px-4 py-2 bg-white text-base font-medium text-gray-700 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                         <font-awesome-icon icon="times" class="text-yellow-500 hover:text-yellow-600"/>
